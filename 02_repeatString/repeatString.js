ac67e56a2e52271ac0fd8803007aafc0b1e94736
kdoni <chrisvdoni@gmail.com>
2022-05-26 15:59:04 +0300

    JS Exercises: TheOdinProject: RepeatString.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    if (num===0){
+        return("");
+    }else if (num<0){
+        return("ERROR")
+    }
+
+    let numero = string;
+    for (n=0;n<num-1;n++){
+        string=string+numero;
+    }
+    return(string);
 
 };
+
+let result = repeatString("hey",10);
 
 // Do not edit below this line
 module.exports = repeatString;
