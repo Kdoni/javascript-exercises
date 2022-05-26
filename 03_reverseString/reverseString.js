93b26aeecbb1084ac15a139d75a30c6d50bbb5d2
kdoni <chrisvdoni@gmail.com>
2022-05-26 17:56:11 +0300

    JS Practice: TheOdinProject: reverseString.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    let splitted = input.split("");
+    let list = [];
+    for (i=splitted.length-1;i>=0;i--){
+        list.push(splitted[i]);
+    }
+    return list.join("");
+
 
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+
+
