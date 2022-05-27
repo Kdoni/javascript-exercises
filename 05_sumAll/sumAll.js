74db29962695e2da06e3a143a193d6eec175082d
kdoni <chrisvdoni@gmail.com>
2022-05-27 12:35:52 +0300

    JS Practice: TheOdinProject: sumAll.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let sum=0;
+    let num1Cor;
+    let num2Cor;
+    if (num1<0 || num2<0){
+        return "ERROR";
+    }else if (typeof num1 == "string" || typeof num2== "string"){
+        return "ERROR";
+    }else if (num1<num2){
+        num1Cor=num1;
+        num2Cor=num2;
+    }else if (num1>num2){
+        num1Cor=num2;
+        num2Cor=num1;
+    }else{
+        return "ERROR";
+    }
+    for (let i=num1Cor;i<=num2Cor;i++){
+        sum +=i;
+    }
+    return sum;
 
 };
 
