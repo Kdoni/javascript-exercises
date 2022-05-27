d6151974df06871e68677100e824ab067174f530
kdoni <chrisvdoni@gmail.com>
2022-05-27 11:45:34 +0300

    JS Practice: TheOdinProject:removeFromArray.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArray,...otherArg) {
+    let filter = myArray.filter(removableObject);
+    function removableObject(eraser){
+        if(otherArg.constructor!==Array){
+            return otherArg!==eraser;
+        }else{
+            return otherArg.includes(eraser) ==false;
+        }
+    }   
+    return filter;
 };
 
 // Do not edit below this line
