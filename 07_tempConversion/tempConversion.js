52936118140b46e8708a169c631a6e34bdc24c3f
kdoni <chrisvdoni@gmail.com>
2022-05-27 15:44:55 +0300

    JS Practice: TheOdinProject: tempConversion.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temperatureF) {
+  let outcome = (temperatureF-32)/1.8;
+  let rounded = Math.round(outcome*10)/10;
+  return rounded;
 };
 
-const ctof = function() {
-
+const ctof = function(temperatureC) {
+  let outcome = (temperatureC*1.8)+32;
+  let rounded = Math.round(outcome*10)/10;
+  return rounded;
 };
 
 // Do not edit below this line
