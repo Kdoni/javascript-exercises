99ad4dec57c7ac094f3ee85f5727d1f6dd461b4c
kdoni <chrisvdoni@gmail.com>
2022-08-20 19:15:06 +0300

    Palindromes for once again...

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    var normal = word.replace(/[^a-zA-Z0-9 ]/g, '', "").toLowerCase().split(" ").join("");
+    var reversed = normal.split("").reverse().join("");
+    return normal === reversed;
 };
 
 // Do not edit below this line
