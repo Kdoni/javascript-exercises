c37324e511ba8df745142a5d8d6bd7d68b183f6f
kdoni <chrisvdoni@gmail.com>
2022-08-21 18:59:40 +0300

    getting used to objects...kinda...

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+    // let sorted = array.sort((a, b) => {
+    //     let itemOne = a.yearOfDeath - a.yearOfBirth;
+    //     let itemTwo = b.yearOfDeath - b.yearOfBirth;
+    //     return itemOne > itemTwo ? -1 : 1;
+    // });
+    // return sorted;
 
+    for (let i = 0; i < array.length; i++) {
+        if ("yearOfDeath" in array[i] === false) {
+            array[i].yearOfDeath = new Date().getFullYear();
+        }
+    }
+    sorted = array.sort((a, b) => a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth ? -1 : 1);
+    return sorted[0];
 };
 
 // Do not edit below this line
