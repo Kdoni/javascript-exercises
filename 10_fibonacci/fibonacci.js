36c1693419849b909a5da07a5018da51ef199577
kdoni <chrisvdoni@gmail.com>
2022-08-21 13:02:12 +0300

    Fibonacci sequence.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+    let progression = 2;
+    let emptyArray = [1, 1];
+    if (number < 0) {
+        return "OOPS";
+    } else if (number === 0) {
+        return 0;
+    } else if (number === 1 || number === 2) {
+        return 1;
+    }
+    for (let i = 2; i < number; i++) {
+        emptyArray.push(progression);
+        progression += emptyArray[i - 1]
+    }
+    return emptyArray[emptyArray.length - 1];
 };
 
 // Do not edit below this line
